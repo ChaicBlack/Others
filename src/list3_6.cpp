58a56317f5e52baa7d427285c35a51655b6b72d0
Bruce Shen <98313918+ChaicBlack@users.noreply.github.com>
2024-04-07 11:51:41 +0100

    std::adopt_lock and std::scope_lock

--- src/list3_6.cpp
+++ src/list3_6.cpp
@@ -14,8 +14,12 @@ public:
     if (&lhs == &rhs)
       return;
     std::lock(lhs.m, rhs.m);
+    // std::adopt_lock is for adopting existing lock
     std::lock_guard<std::mutex> lock_a(lhs.m, std::adopt_lock);
     std::lock_guard<std::mutex> lock_b(rhs.m, std::adopt_lock);
+    // After C++17, using scope_lock is a wiser choise
+    // 16, 18 => written:
+    // std::scope_lock lock(lhs.m, rhs.m);
     std::swap(lhs.some_detail, rhs.some_detail);
   }
 };
