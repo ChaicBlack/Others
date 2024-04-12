7bf0e50d34f0c8d320366c59b594ef26785f9be2
Bruce Shen <98313918+ChaicBlack@users.noreply.github.com>
2024-04-12 22:01:19 +0100

    listing4.8 completed

---
+++ src/list4_8.cpp
@@ -0,0 +1,7 @@
+// 这是一个std::packaged_task<>的definition
+template <> class packaged_task<std::string(std::vector<char> *, int)> {
+public:
+  template <typename Callable> explicit packaged_task(Callable &&f);
+  std::future<std::string> get_future();
+  void operator()(std::vector<char> *, int);
+};
