027c45651ab98f6c1395c14bf8e07c7fff89d210
Bruce Shen <98313918+ChaicBlack@users.noreply.github.com>
2024-04-24 22:16:07 +0100

    listing4.25 complete

--- src/list4_25.cpp
+++ src/list4_25.cpp
@@ -5,6 +5,7 @@ void foo() {
   my_data data[thread_count];
   std::vector<std::future<void>> threads;
   for (int i = 0; i < thread_count; i++) {
+    // 除了i都捕获引用，为防止data race所以不能捕获i
     threads.push_back(std::async(std::launch::async, [&, i] {
       data[i] = make_data(i);
       done.count_down();
