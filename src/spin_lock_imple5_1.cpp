936e02754ec441b2137f7bb84aa9b12ace848efb
Bruce Shen <98313918+ChaicBlack@users.noreply.github.com>
2024-04-28 18:21:54 +0100

    some change

--- src/spin_lock_imple5_1.cpp
+++ src/spin_lock_imple5_1.cpp
@@ -4,12 +4,12 @@
 #include <atomic>
 class spinlock_mutex {
   std::atomic_flag flag;
+
 public:
   spinlock_mutex() : flag(ATOMIC_FLAG_INIT) {}
   void lock() {
-    while(flag.test_and_set(std::memory_order_acquire));
+    while (flag.test_and_set(std::memory_order_acquire))
+      ;
   }
-  void unlock() {
-    flag.clear(std::memory_order_release);
-  }
+  void unlock() { flag.clear(std::memory_order_release); }
 };
